92428d23436c6b9a2d31b97cfaf04a8816b6cb43
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-22 22:33:09 +0900

    add all files

---
+++ zombies_code/1.makingZombieFactory/3.const/example.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.4.24;
+
+contract ZombieFactory {
+    // この部分にdnaDigitsを定義する
+    uint dnaDigits = 16;
+}
---
+++ zombies_code/1.makingZombieFactory/4.plus/example.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.4.24;
+
+contract ZombieFactory {
+    uint dnaDigits = 16;
+
+    // ここにdnaModulusを定義
+}
